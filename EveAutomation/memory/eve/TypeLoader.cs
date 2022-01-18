216e1e0b4463ea84ef291cb6608ad00b6ac5d4e7
CallowBlack <stroganov-oleges@yandex.ru>
2022-01-18 16:11:38 +0300

    Add eve types

---
+++ EveAutomation/memory/eve/TypeLoader.cs
@@ -0,0 +1,18 @@
+﻿using EveAutomation.memory.eve.type;
+using EveAutomation.memory.python;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveAutomation.memory.eve
+{
+    public static class TypeLoader
+    {
+        public static void Initialize()
+        {
+            PyObjectTypeDeterminer.AddTypeConstructor("Bunch", address => new Bunch(address));
+        }
+    }
+}
---
+++ EveAutomation/memory/eve/type/Bunch.cs
@@ -0,0 +1,16 @@
+﻿using EveAutomation.memory.python.type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveAutomation.memory.eve.type
+{
+    public class Bunch : PyDict
+    {
+        public Bunch(ulong address) : base(address)
+        {
+        }
+    }
+}
