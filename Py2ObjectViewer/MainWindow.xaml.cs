9564ed502641c73f0afa2bb8c5a3b31b45af46f1
CallowBlack <stroganov-oleges@yandex.ru>
2022-01-21 00:24:46 +0300

    Fix performance issue and refactor code

--- Py2ObjectViewer/MainWindow.xaml.cs
+++ Py2ObjectViewer/MainWindow.xaml.cs
@@ -44,32 +44,30 @@ namespace Py2ObjectViewer
         {
             while (true)
             {
-                DeepUpdate();
+                UpdateLoaded();
                 await Task.Delay(1000);
             }
         }
 
-        public async void DeepUpdate()
+        public void UpdateLoaded()
         {
             UpdateCount = 0;
-            foreach (var item in PyObjectsWrapped)
-                if (item.Origin is PyObject pyObject)
-                    pyObject.Update(true, true);
+            List<PyObject> loadedClone = new(PyObjectWrapper.loadedObjects);
+            foreach (var item in loadedClone)
+                item.Update(true);
         }
 
         public void SearchItems(string name, bool contains = true)
         {
+            PyObjectWrapper.loadedObjects.Clear();
+
             PyObjectPool.ScanProcessMemory(new List<String>() { name }, contains);
-            PyObjectsWrapped = new ObservableCollection<PyObjectCollectionWrapper>(PyObjectPool.GetObjects()
-                .Select(item => new PyObjectCollectionWrapper(item)));
+            PyObjectsWrapped = new ObservableCollection<PyObjectWrapper>(PyObjectPool.GetObjects()
+                .Select(item => new PyObjectWrapper(item)));
 
             foreach(var item in PyObjectPool.GetObjects())
             {
                 item.FieldChanged += Item_FieldChanged;
-            }
-            foreach(var obj in PyObjectsWrapped)
-            {
-                obj.Load();
             }
         }
 
@@ -80,7 +78,8 @@ namespace Py2ObjectViewer
 
         public string SearchText { get; set; }
 
-        public ObservableCollection<PyObjectCollectionWrapper> PyObjectsWrapped {
+        public ObservableCollection<PyObjectWrapper> PyObjectsWrapped 
+        {
             get => _pyObjectWrapped;
             set
             {
@@ -88,15 +87,19 @@ namespace Py2ObjectViewer
                 NotifyPropertyChanged();
             }
         }
+        private ObservableCollection<PyObjectWrapper>? _pyObjectWrapped;
 
-        public int UpdateCount { get => _updateCount;
-        set { _updateCount = value;
+        public int UpdateCount 
+        { 
+            get => _updateCount;
+
+            set 
+            { 
+                _updateCount = value;
                 NotifyPropertyChanged();
             }
         }
         private int _updateCount;
-
-        private ObservableCollection<PyObjectCollectionWrapper>? _pyObjectWrapped;
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -108,266 +111,6 @@ namespace Py2ObjectViewer
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SearchItems(SearchText);
-        }
-    }
-
-    [ValueConversion(typeof(PyObjectCollectionWrapper), typeof(PyObjectCollectionWrapper))]
-    public class PyCollectionInitializerConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is PyObjectCollectionWrapper collection)
-                collection.Load();
-
-            return value;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    [ValueConversion(typeof(IWrapper), typeof(string))]
-    public class PyObjectToStringConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var origin = value;
-            if (origin is PyDict dict)
-                return $"dict[{dict.Length}]";
-            else if (origin is PyList list)
-                return $"list[{list.Length}]";
-            else if (origin is PyTuple tuple)
-                return $"tuple[{tuple.Length}]";
-            else if (origin is PyString || origin is PyUnicode || origin is PyInt || origin is PyFloat || origin is PyBool)
-                return origin?.ToString() ?? "<error>";
-            else if (origin is PyObject obj) {
-                if (obj.Type.Name == "NoneType")
-                    return "None";
-
-                if (obj.Dict == null)
-                    return $"dict[0]";
-
-                var name = obj.Dict.Get("_name") as PyString;
-                if (name != null)
-                    return $"dict[{obj.Dict.Length}]:{name.Value}";
-
-                var _childrenObjects = obj.Dict.Get("_childrenObjects") as PyList;
-                if (_childrenObjects != null)
-                    return $"list[{_childrenObjects.Length}]";
-
-                return $"dict[{obj.Dict.Length}]";
-            }
-            return "undefined";
-        }
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
-
-    public class KeyValue
-    {
-        public string Key { get; set; }
-        public IWrapper Value { get; set; }
-
-        public KeyValue(string key, IWrapper value)
-        {
-            Key = key;
-            Value = value;
-        }
-    }
-
-    public interface IWrapper
-    {
-        public object Origin { get; set; }
-
-        public string Key { get; set; }
-    }
-
-
-    public static class WrapperPool {
-        public static Dictionary<object, IWrapper> wrappers = new();
-    }
-    public class PyKeyValueWrapper : INotifyPropertyChanged, IWrapper
-    {
-        public object Origin { get; set; }
-
-        public PyObject Key { get; private set; }
-
-        public IWrapper Value { get; private set; }
-        string IWrapper.Key { get => Key.ToString(); set { } }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public PyKeyValueWrapper(KeyValuePair<PyObject, PyObject> pair)
-        {
-            Origin = pair;
-
-            Key = pair.Key;
-
-            if (WrapperPool.wrappers.ContainsKey(pair.Value))
-            {
-                Value = WrapperPool.wrappers[pair.Value];
-            }
-            else if (pair.Value is IValueChanged valueChanged)
-            {
-                Value = new PyObjectValueWrapper(valueChanged);
-                valueChanged.ValueChanged += ValueChanged_ValueChanged;
-            }
-            else if (pair.Value is PyList pyList)
-            {
-                Value = new PyObjectCollectionWrapper(pyList);
-            }
-            else
-            {
-                Value = new PyObjectCollectionWrapper(pair.Value);
-            }
-        }
-
-        private void ValueChanged_ValueChanged(ValueChangedArgs args)
-        {
-            PropertyChanged?.Invoke(this, new(null));
-        }
-    }
-
-    public class PyObjectValueWrapper : INotifyPropertyChanged, IWrapper
-    {
-        public object Origin { get; set; }
-
-        public string Key { get; set; }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public PyObjectValueWrapper(IValueChanged obj, string? key = null)
-        {
-            Origin = obj;
-            Key = key ?? "<NoName>";
-            obj.ValueChanged += OnValueChanged;
-        }
-
-        private void OnValueChanged(ValueChangedArgs args)
-        {
-            PropertyChanged?.Invoke(this, new(""));
-        }
-    }
-
-    public class PyObjectCollectionWrapper : ObservableCollection<IWrapper>, IWrapper
-    {
-        public object Origin { get; set; }
-
-        public string Key { get; set; }
-
-        public ObservableCollection<IWrapper> Wrappers { get => this; }
-
-
-        private bool _isLoaded = false;
-
-        public void Load()
-        {
-            if (_isLoaded)
-                return;
-            WrapperPool.wrappers[Origin] = this;
-            if (Origin is PyList list)
-            {
-                AddItems(new List<PyObject>(list.Items));
-                list.ListChanged += OnListChanged;
-            }
-            else if (Origin is PyTuple tuple)
-                AddItems(tuple.Items);
-            else if (Origin is PyDict dict)
-            {
-                AddItems(dict.Items);
-                dict.DictionaryChanged += OnDictionaryChanged;
-            }
-            else if (Origin is PyObject obj && obj.Dict != null)
-            {
-                AddItems(obj.Dict.Items);
-                obj.Dict.DictionaryChanged += OnDictionaryChanged;
-            }
-            _isLoaded = true;
-        }
-
-        public PyObjectCollectionWrapper(PyObject pyObject, string? name = null) {
-            Key = name ?? "<NoName>";
-            Origin = pyObject;
-        }
-
-        public void AddItems(IEnumerable<PyObject> items)
-        {
-            var i = 0;
-            foreach (var item in items)
-            {
-                var currKey = $"[{i}]";
-                if (WrapperPool.wrappers.ContainsKey(item))
-                {
-                    Add(WrapperPool.wrappers[item]);
-                }
-                else if (item is IValueChanged vc)
-                {
-                    Add(new PyObjectValueWrapper(vc, currKey));
-                }
-                else
-                {
-                    Add(new PyObjectCollectionWrapper(item, currKey));
-                }
-                i++;
-            }
-        }
-
-        public void AddItems(IEnumerable<KeyValuePair<PyObject, PyObject>> items)
-        {
-            foreach (var item in items)
-            {
-                if (WrapperPool.wrappers.ContainsKey(item))
-                {
-                    Add(WrapperPool.wrappers[item]);
-                }
-                else
-                {
-                    Add(new PyKeyValueWrapper(item));
-                }
-            }
-        }
-
-        public void RemoveItems(IEnumerable items)
-        {
-            foreach (var item in items)
-            {
-                try
-                {
-                    var itemToRemove = this.First(value => value.Origin == item);
-                    Remove(itemToRemove);
-                }
-                catch (InvalidOperationException) { }
-            }
-        }
-
-        private void OnListChanged(CollectionChangedArgs<PyObject> args)
-        {
-            if (args.AddedItems != null)
-            {
-                AddItems(args.AddedItems);
-            }
-
-            if (args.RemovedItems != null)
-            {
-                RemoveItems(args.RemovedItems);
-            }
-        }
-
-        private void OnDictionaryChanged(CollectionChangedArgs<KeyValuePair<PyObject, PyObject>> args)
-        {
-            if (args.AddedItems != null)
-            {
-                AddItems(args.AddedItems);
-            }
-
-            if (args.RemovedItems != null)
-            {
-                RemoveItems(args.RemovedItems);
-            }
         }
     }
 }
---
+++ Py2ObjectViewer/PyObjectWrapper.cs
@@ -0,0 +1,175 @@
+﻿using EveAutomation.memory.python;
+using EveAutomation.memory.python.type;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Py2ObjectViewer
+{
+    public class PyObjectWrapper : INotifyPropertyChanged
+    {
+        public static readonly HashSet<PyObject> loadedObjects = new();
+
+        public PyObject Origin { get; private set; }
+        public string Key { get; protected set; }
+
+        public string Presentation { get => GetPresentation(); }
+
+        public ObservableCollection<PyObjectWrapper> Wrappers {
+            get
+            {
+                if (_wrappers == null)
+                    _wrappers = new(LoadWrappers());
+
+                return _wrappers;
+            }
+        }
+        private ObservableCollection<PyObjectWrapper>? _wrappers;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public PyObjectWrapper(PyObject pyObject, string? key = null)
+        {
+            Key = key ?? "object";
+            Origin = pyObject;
+            
+            if (Origin is IValueChanged vc)
+            {
+                vc.ValueChanged += OnValueChanged;
+            }
+
+            loadedObjects.Add(Origin);
+        }
+
+        private string GetPresentation()
+        {
+            if (Origin is PyDict dict)
+                return $"dict[{dict.Length}]";
+            else if (Origin is PyList list)
+                return $"list[{list.Count}]";
+            else if (Origin is PyTuple tuple)
+                return $"tuple[{tuple.Count}]";
+            else if (Origin is PyString || Origin is PyUnicode || Origin is PyInt || Origin is PyFloat || Origin is PyBool)
+                return Origin?.ToString() ?? "<error>";
+            else if (Origin is PyObject obj)
+            {
+                if (obj.Type.Name == "NoneType")
+                    return "None";
+
+                if (obj.Dict == null)
+                    return $"dict[0]";
+
+                if (obj.Dict.Get("_name") is PyString name)
+                    return $"dict[{obj.Dict.Length}]:{name.Value}";
+
+                if (obj.Dict.Get("_childrenObjects") is PyList _childrenObjects)
+                    return $"list[{_childrenObjects.Count}]";
+
+                return $"dict[{obj.Dict.Length}]";
+            }
+            return "undefined";
+        }
+
+        private IEnumerable<PyObjectWrapper> LoadWrappers()
+        {
+            if (Origin is IValueChanged)
+                yield break;
+
+            if (Origin is PyCollection list)
+            {
+                uint i = 0;
+                foreach (var item in list.Items)
+                {
+                    yield return new PyObjectWrapper(item, $"[{i}]");
+                    i++;
+                }
+            }
+            else
+            {
+                var dict = Origin is PyDict pyDict ? pyDict : Origin.Dict;
+                if (dict == null)
+                    yield break;
+
+                foreach (var item in dict.Items)
+                {
+                    yield return new PyObjectWrapper(item.Value, item.Key.ToString());
+                }
+            }
+
+            UpdateEvent();
+        }
+
+        private void UpdateEvent()
+        {
+            if (Origin is PyList list)
+            {
+                list.ListChanged += OnListChanged;
+            }
+            else if (Origin is PyDict dict)
+            {
+                dict.DictionaryChanged += OnDictChanged;
+            }
+            else if (Origin.Dict != null)
+            {
+                Origin.Dict.DictionaryChanged += OnDictChanged;
+            }
+        }
+
+        private void OnValueChanged(ValueChangedArgs args)
+        {
+            PropertyChanged?.Invoke(this, new(null));
+        }
+
+        private void OnDictChanged(CollectionChangedArgs<KeyValuePair<PyObject, PyObject>> args)
+        {
+            if (args.AddedItems != null) AddItems(args.AddedItems.Select(pair => pair.Value));
+
+            if (args.RemovedItems != null) RemoveItems(args.RemovedItems.Select(pair => pair.Value));
+        }
+
+        private void OnListChanged(CollectionChangedArgs<PyObject> args)
+        {
+            if (args.AddedItems != null) AddItems(args.AddedItems);
+
+            if (args.RemovedItems != null) RemoveItems(args.RemovedItems);
+        }
+
+        private void RemoveItems(IEnumerable<PyObject> pyObjects)
+        {
+            var indexes = FindIndexes(pyObjects);
+            foreach (var index in indexes)
+                Wrappers.RemoveAt(index);
+
+            if (Origin is PyCollection && indexes.Any())
+                ReindexWrappers(indexes.Min());
+        }
+
+        private void AddItems(IEnumerable<PyObject> pyObjects)
+        {
+            var isCollection = Origin is PyCollection;
+            foreach (var pyObject in pyObjects)
+                Wrappers.Add(new PyObjectWrapper(pyObject, isCollection ? $"[{Wrappers.Count}]" : null));
+        }
+
+        private IEnumerable<int> FindIndexes(IEnumerable<PyObject> pyObjects)
+        {
+            var set = new HashSet<PyObject>(pyObjects);
+            for (int i = 0; i < Wrappers.Count; i++)
+            {
+                if (set.Contains(Wrappers[i].Origin))
+                    yield return i;
+            }
+        }
+
+        private void ReindexWrappers(int startIndex)
+        {
+            for (int i = startIndex; i < Wrappers.Count; i++)
+                Wrappers[i].Key = $"[{i}]";
+        }
+    }
+}
